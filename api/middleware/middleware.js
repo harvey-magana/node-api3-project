ff1d849123d39eed7e3f673aac917b7d23511282
harvey magana <heyharv2012@gmail.com>
2021-01-21 19:29:16 -0800

    Started the code for index.js so the server can work.

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,9 +1,24 @@
 function logger(req, res, next) {
   // do your magic!
+
+  /**
+   * logs to the console the following information about each request:
+   * request method
+   * request url
+   * timestamp
+   * this middleware runs on every request made to the API
+   */
 }
 
 function validateUserId(req, res, next) {
   // do your magic!
+
+  /**
+   * this middleware will be used for all user endpoints that include an id parameter
+   * in the url, 
+   * example, /api/users/:id
+   * it should check the database to make sure there is a user with that id
+   */
 }
 
 function validateUser(req, res, next) {
