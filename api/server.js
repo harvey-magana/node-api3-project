f8aa5d8f58c19dbe78e80869647b4109713387e3
harvey magana <heyharv2012@gmail.com>
2021-01-21 19:34:19 -0800

    Updated the server file to include the Route variables.

--- api/server.js
+++ api/server.js
@@ -6,6 +6,17 @@ const server = express();
 
 // global middlewares and routes need to be connected here
 
+const PostRouter = require('./posts/posts-router.js');
+
+const UserRouter = require('./users/users-router.js');
+
+server.use(express.json());
+
+server.use('/api/posts', PostRouter);
+server.use('/api/posts/:id', PostRouter);
+server.use('/api/users/', UserRouter);
+server.use('/api/users/:id', UserRouter);
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
